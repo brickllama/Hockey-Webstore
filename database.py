e8ece8324a83f28d54787c1f0e35f56c0e760bf3
drbrickllama <brickllama@oci.scottn.us>
2023-11-12 05:00:30 +0000

    get real database secrets from environment but keep defaults for dev.

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import os
 
 mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    port="3006",
-    user="root",
-    password="demo",
-    database="testdatabase"
+    host=os.getenv("MYSQL_HOST", "127.0.0.1"),
+    port=os.getenv("MYSQL_PORT", "3006"),
+    user=os.getenv("MYSQL_USER", "root"),
+    password=os.getenv("MYSQL_PASS", "demo"),
+    database=os.getenv("MYSQL_DB", "testdatabase")
 )
 
 mycursor = mydb.cursor()
